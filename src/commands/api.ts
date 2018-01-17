7df268ce1a82b1f0ce7975e499773ce99f15cc48
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2018-01-17 19:23:33 +0530

    Add /robohash and /zalgo.

--- src/commands/api.ts
+++ src/commands/api.ts
@@ -18,6 +18,14 @@ export function handleUrban (message: string, sendResponse: Function) {
     })
 }
 
+export function handleZalgo (message: string, sendResponse: Function) {
+  // Fetch a zalgo.
+  fetch(`http://zalgo.io/api?text=${getArguments(message)}`)
+    .then(res => res.text())
+    .catch(err => sendResponse(`Something went wrong 👾 Error: ${err}`))
+    .then(text => sendResponse(text))
+}
+
 export function handleCat (message: string, sendResponse: Function) {
   // Fetch a cat.
   fetch(`http://random.cat/meow`)
@@ -38,4 +46,19 @@ export function handleDog (message: string, sendResponse: Function) {
     .then(res => res.json())
     .catch(err => sendResponse(`Something went wrong 👾 Error: ${err}`))
     .then(json => sendResponse(json.message))
+}
+
+export function handleRobohash (message: string, sendResponse: Function) {
+  // Get text to hash.
+  let text: string|Array<string> = getArguments(message).split(' ')
+  text.splice(0, 1)
+  text = text.join('%20')
+  // Send a robohash.
+  if (getArguments(message).split(' ')[0] === 'robot') sendResponse(`https://robohash.org/${text}.png`)
+  else if (getArguments(message).split(' ')[0] === 'monster') sendResponse(`https://robohash.org/${text}.png?set=set2`)
+  else if (getArguments(message).split(' ')[0] === 'head') sendResponse(`https://robohash.org/${text}.png?set=set3`)
+  else if (getArguments(message).split(' ')[0] === 'cat') sendResponse(`https://robohash.org/${text}.png?set=set4`)
+  else {
+    sendResponse('Proper usage: /robohash <robot, monster, head, cat> <text to robohash>')
+  }
 }
