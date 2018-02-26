9bdc79c80920d8d5be5ce1afca6943eb58e27318
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2018-02-26 16:45:40 +0530

    Pave way for server specific settings.

--- server/resolvers.ts
+++ server/resolvers.ts
@@ -3,13 +3,24 @@ export default {
   // Queries.
   Query: {
     // Warns of user.
-    allWarnsOfUser: (parent, { userId }, ctx, info) => ctx.db.query.warningDiscords({
-      where: { warnedId: userId }
+    allWarnsOfUser: (parent, { userId, serverId }, ctx, info) => ctx.db.query.warningDiscords({
+      where: { warnedId: userId, serverId }
+    }, info),
+    serverSettings: (parent, { serverId }, ctx, info) => ctx.db.query.serverSettings({
+      where: { serverId }
     }, info)
   },
   Mutation: {
     warn: (parent, {warnedId, warnerId, reason}, ctx, info) => ctx.db.mutation.createWarningDiscord(
       { data: { warnedId, warnerId, reason, date: new Date().toUTCString() } },
+      info
+    ),
+    editServerSettings: (parent, { serverId, addRoleForAll }, ctx, info) => ctx.db.mutation.updateManyServerSettings(
+      { data: { addRoleForAll }, where: { serverId } },
+      info
+    ),
+    initServerSettings: (parent, { serverId }, ctx, info) => ctx.db.mutation.createServerSetting(
+      { data: { serverId } },
       info
     )
   }
