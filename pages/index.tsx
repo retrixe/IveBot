79dbeca87c2ca1fa14a10ffeaa194e17c3dfb6b8
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2018-03-02 15:11:02 +0530

    Add the basics to the web dashboard.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
 import * as React from 'react'
+import { AppBar, Toolbar } from 'material-ui'
+import Typography from 'material-ui/Typography'
 
+/* eslint-disable quotes, no-multi-str */
 const Index = () => (
-  <div>Hello world!</div>
+  <>
+    <AppBar>
+      <Toolbar>
+        <Typography variant='title' color='inherit'>IveBot</Typography>
+      </Toolbar>
+    </AppBar>
+    <br /><br /><br /><br />
+    <Typography>
+      IveBot is just a Discord bot. A Discord bot that kills other Discord bots.
+    </Typography>
+  </>
 )
+/* eslint-enable quotes, no-multi-str */
 
 export default Index
